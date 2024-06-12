d3765a367cb1f2ebe6d236564fd396f0cb30a10a
llAndroidManll <sahakyansahak404@gmail.com>
2024-06-12 17:45:30 +0400

    Creating RunningApp class which inherits from Application class and overriding onCreate func for creating notification channel

--- app/src/main/java/com/example/foregroundservice/RunningApp.kt
+++ app/src/main/java/com/example/foregroundservice/RunningApp.kt
@@ -1,4 +1,36 @@
 package com.example.foregroundservice
 
-class RunningApp {
+import android.app.Application
+import android.app.NotificationChannel
+import android.app.NotificationManager
+import android.content.Context
+import android.os.Build
+
+/*TODO: 8. Creating Application class
+* */
+class RunningApp : Application() {
+
+    /*TODO: 9. Overriding onCreate method
+    *
+    *   Check if is our application running on Android Oreo,
+    *   and if it is running, we are going to create a notification channel.
+    **/
+
+    override fun onCreate() {
+        super.onCreate()
+
+        if (Build.VERSION.SDK_INT >= Build.VERSION_CODES.O) {
+            val channel = NotificationChannel(
+                "running_channel",
+                "Running Notifications",
+                NotificationManager.IMPORTANCE_HIGH
+            )
+
+            val notificationManager =
+                getSystemService(Context.NOTIFICATION_SERVICE) as NotificationManager
+            notificationManager.createNotificationChannel(channel)
+
+        }
+    }
+
 }
