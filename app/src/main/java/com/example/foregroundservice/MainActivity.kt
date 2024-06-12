8bc5ff636a522c187ed21b595db55e5ac53dbbb1
llAndroidManll <sahakyansahak404@gmail.com>
2024-06-12 17:52:44 +0400

    Requesting Permissions, Displaying Buttons and then sending intent to the foreground service

--- app/src/main/java/com/example/foregroundservice/MainActivity.kt
+++ app/src/main/java/com/example/foregroundservice/MainActivity.kt
@@ -1,25 +1,80 @@
 package com.example.foregroundservice
 
+import android.content.Intent
+import android.os.Build
 import android.os.Bundle
 import androidx.activity.ComponentActivity
 import androidx.activity.compose.setContent
 import androidx.activity.enableEdgeToEdge
+import androidx.annotation.RequiresApi
+import androidx.compose.foundation.layout.Arrangement
+import androidx.compose.foundation.layout.Column
 import androidx.compose.foundation.layout.fillMaxSize
 import androidx.compose.foundation.layout.padding
+import androidx.compose.material3.Button
 import androidx.compose.material3.Scaffold
 import androidx.compose.material3.Text
 import androidx.compose.runtime.Composable
+import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
 import androidx.compose.ui.tooling.preview.Preview
+import androidx.compose.ui.unit.dp
+import androidx.core.app.ActivityCompat
 import com.example.foregroundservice.ui.theme.ForegroundServiceTheme
 
 class MainActivity : ComponentActivity() {
+
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-        enableEdgeToEdge()
+
+
+        /*TODO: 10. Requesting permission
+        * */
+        if (Build.VERSION.SDK_INT >= Build.VERSION_CODES.TIRAMISU) {
+            ActivityCompat.requestPermissions(
+                this,
+                arrayOf(android.Manifest.permission.POST_NOTIFICATIONS),
+                0
+            )
+        }
+
+
         setContent {
             ForegroundServiceTheme {
-
+                Column(
+                    modifier = Modifier
+                        .fillMaxSize()
+                        .padding(16.dp),
+                    horizontalAlignment = Alignment.CenterHorizontally,
+                    verticalArrangement = Arrangement.Center
+                ) {
+                    /*TODO: 11. Displaying two buttons to start and stop the service
+                    * */
+                    Button(
+                        onClick = {
+                            /*TODO: 12. Sending intent to the our foreground service with our custom actions
+                            *
+                            * */
+                            Intent(applicationContext, RunningService::class.java).also {
+                                it.action = RunningService.Actions.START.toString()
+                                startService(it)
+                            }
+                        }
+                    ) {
+                        Text(text = "Start run")
+                    }
+                    Button(
+                        onClick = {
+                            Intent(applicationContext, RunningService::class.java).also {
+                                it.action = RunningService.Actions.STOP.toString()
+                                startService(it)
+                            }
+                        }
+                    ) {
+                        Text(text = "Stop run")
+                    }
+                }
             }
         }
     }
